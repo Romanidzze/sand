0b67d08965ad20aaeab367dfb81f07e1722a6511
Roman Baran <baranroma70@gmail.com>
2024-03-04 12:22:16 +0300

    fixed

--- sandproj.cpp
+++ sandproj.cpp
@@ -6,19 +6,7 @@ int main()
 {
     setlocale(0, "");
     char alphabet[] = "ABCDEFGHIJKLMNOPQRSTVWXYZ";
-    string pers[] = { "Abaddon", "Alchemist", "Ancient Apparition", "Anti - Mage", "Arc Warden", "Axe",\
-                      "Bane", "Batrider", "Beastmaster", "Bloodseeker", "Bounty Hunter", "Brewmaster", "Bristleback", "Broodmother",\
-                      "Centaur Warrunner", "Chaos Knight", "Chen", "Clinkz", "Clockwerk", "Crystal Maiden",\
-                      "Dark Seer", "Dark Willow", "Dawnbreaker", "Dazzle", "Death Prophet", "Disruptor", "Doom", "Dragon Knight", "Drow Ranger",\
-                      "Earth Spirit", "Earthshaker", "Elder Titan", "Ember Spirit", "Enchantress", "Enigma",\
-                      "Faceless Void", "Grimstroke", "Gyrocopter", "Hoodwink", "Huskar", "Invoker", "Io", "Jakiro", "Juggernaut",\
-                      "Keeper of the Light", "Kunkka", "Legion Commander", "Leshrac", "Lich", "Lifestealer", "Lina", "Lion", "Lone Druid", "Luna", "Lycan",\
-        'Bulbochka'
-                      "Queen of Pain", "Razor", "Riki", "Rubick", "Sand King", "Shadow Demon", "Shadow Fiend",\
-                      "Shadow Shaman", "Silencer", "Skywrath Mage", "Slardar", "Slark", "Sniper", "Spectre", "Spirit Breaker", "Storm Spirit", "Sven",\
-                      "Techies", "Templar Assassin", "Terrorblade", "Tidehunter", "Timbersaw", "Tinker", "Tiny", "Treant Protector", "Troll Warlord", "Tusk",\
-                      "Underlord", "Undying", "Ursa", "Vengeful Spirit", "Venomancer", "Viper", "Visage", "Void Spirit",\
-                      "Warlock", "Weaver", "Windranger", "Winter Wyvern", "Witch Doctor", "Wraith King", "Zeus" };
+    string pers[] = { "Abaddon" }
     srand(time(NULL));
     int index_alpha = rand() % 25;
     int size_pers = sizeof(pers) / 40;
